079fe9216061ce80bce2794094dc7082e94a1a6d
Chirag Sehgal <chirag23176@iiitd.ac.in>
2025-02-12 00:32:18 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import torch
+from torch.autograd.function import _SingleLevelFunction
 import torch.nn as nn
 import torch.nn.functional as F
 
 #prediction models
 class fashionModel1(nn.Module):
   def __init__(self):
-    super().__init__()              #1x28x28
+    super(fashionModel1,self).__init__()              #1x28x28
     self.conv1 = nn.Conv2d(1,6,5)   #6x24x24
     self.pool1 = nn.AvgPool2d(2,2)  #6x12x12
     self.conv2 = nn.Conv2d(6,16,3)  #16x8x8
@@ -24,7 +25,7 @@ class fashionModel1(nn.Module):
 
 class fashionModel2(nn.Module):
   def __init__(self):
-    super().__init__()             #1x28x28
+    super(fashionModel2,self).__init__()             #1x28x28
     self.conv1 = nn.Conv2d(1,3,3)  #6x26x26
     self.pool1 = nn.AvgPool2d(2,2) #3x13x13
     self.conv2 = nn.Conv2d(3,6,3)  #6x11x11 -> 6x5x5
@@ -43,17 +44,18 @@ class fashionModel2(nn.Module):
 
 class speechModel1(nn.Module):
   def __init__(self):
-    super().__init__()              #1x28x28
-    self.conv1 = nn.Conv1d(1,6,5)   #6x24x24
-    self.pool1 = nn.AvgPool1d(2,2)  #6x12x12
-    self.conv2 = nn.Conv1d(6,16,3)  #16x8x8
-    self.dense1= nn.Linear(16*8*8,512)
-    self.dense2= nn.Linear(512,128)
-    self.dense3= nn.Linear(128,10)
+    super(speechModel1,self).__init__()              #1x16k
+    self.conv1 = nn.Conv1d(1,6,7,2) #6x7997
+    self.pool1 = nn.AvgPool1d(2,2)  #6x3998
+    self.conv2 = nn.Conv1d(6,16,3)  #16x3994 -> 16x1997 ->16x998
+    self.dense1= nn.Linear(16*998,1024)
+    self.dense2= nn.Linear(1024,256)
+    self.dense3= nn.Linear(256,32) #64 dimensional output
   
   def forward(self,x):
     x=self.pool1(F.relu(self.conv1(x)))
     x=self.pool1(F.relu(self.conv2(x)))
+    x = self.pool1(x)
     x = torch.flatten(x,1)
     x=F.relu(self.dense1(x))
     x=F.relu(self.dense2(x))
@@ -62,13 +64,14 @@ class speechModel1(nn.Module):
 
 class speechModel2(nn.Module):
   def __init__(self):
-    super().__init__()             #1x28x28
-    self.conv1 = nn.Conv1d(1,3,3)  #6x26x26
-    self.pool1 = nn.AvgPool1d(2,2) #3x13x13
-    self.conv2 = nn.Conv1d(3,6,3)  #6x11x11 -> 6x5x5
-    self.conv3 = nn.Conv1d(6,10,3) #10x3x3
-    self.dense1= nn.Linear(10*3*3,100) 
-    self.dense2= nn.Linear(100,10)
+    super(speechModel2,self).__init__()           #1x16k
+    self.conv1 = nn.Conv1d(1,6,7,2) #6x7997
+    self.pool1 = nn.AvgPool1d(2,2)  #6x3998
+    self.conv2 = nn.Conv1d(6,16,5)  #16x3994 -> 16x1997
+    self.conv3 = nn.Conv1d(16,32,3) #32x1995 ->32x997
+    self.dense1= nn.Linear(16*997,1024)
+    self.dense2= nn.Linear(1024,256)
+    self.dense3= nn.Linear(256,32)
   
   def forward(self,x):
     x=self.pool1(F.relu(self.conv1(x)))
@@ -76,37 +79,22 @@ class speechModel2(nn.Module):
     x=self.pool1(F.relu(self.conv3(x)))
     x = torch.flatten(x,1)
     x=F.relu(self.dense1(x))
-    x=self.dense2(x)
+    x=F.relu(self.dense2(x))
+    x=self.dense3(x)
     return x
-
-def fashionModel1():
-  model =fashionModel1()
-  return model
-
-def fashionModel2():
-  model =fashionModel2()
-  return model
-
-def speechModel1():
-  model = speechModel1()
-  return model
-
-def speechModel2():
-  model = speechModel2()
-  return model
 
 #representation models
 #common loss functions for these include Triplet Loss, Contrastive Loss
 
 class fashionEmbedded(nn.Module):
   def __init__(self):
-    super().__init__()              #1x28x28
+    super(fashionEmbedded,self).__init__()              #1x28x28
     self.conv1 = nn.Conv2d(1,6,5)   #6x24x24
     self.pool1 = nn.AvgPool2d(2,2)  #6x12x12
     self.conv2 = nn.Conv2d(6,16,3)  #16x8x8
     self.dense1= nn.Linear(16*8*8,512)
-    self.dense2= nn.Linear(512,128)
-    self.dense3= nn.Linear(128,32) ##32 dimensional output
+    self.dense2= nn.Linear(512,256)
+    self.dense3= nn.Linear(256,64) #64 dimensional output
   
   def forward(self,x):
     x=self.pool1(F.relu(self.conv1(x)))
@@ -117,19 +105,31 @@ class fashionEmbedded(nn.Module):
     x=self.dense3(x)
     return x
 
+class smallFashionPrediction(nn.Module):
+  def __init__(self):
+    super(smallFashionPrediction,self).__init__()
+    self.dense1= nn.Linear(64,32)
+    self.dense2= nn.Linear(32,10) #64 dimensional output
+
+  def forward(self,x):
+      x=F.relu(self.dense1(x))
+      x=self.dense2(x)
+      return x
+
 class speechEmbedded(nn.Module):
   def __init__(self):
-    super().__init__()              #1x28x28
-    self.conv1 = nn.Conv1d(1,6,5)   #6x24x24
-    self.pool1 = nn.AvgPool1d(2,2)  #6x12x12
-    self.conv2 = nn.Conv1d(6,16,3)  #16x8x8
-    self.dense1= nn.Linear(16*8*8,512)
-    self.dense2= nn.Linear(512,128)
-    self.dense3= nn.Linear(128,64) #64 dimensional output
+    super(speechEmbedded,self).__init__()              #1x16k
+    self.conv1 = nn.Conv1d(1,6,7,2)   #6x7997
+    self.pool1 = nn.AvgPool1d(2,2)  #6x3998
+    self.conv2 = nn.Conv1d(6,16,3)  #16x3994 -> 16x1997 ->16x998
+    self.dense1= nn.Linear(16*998,1024)
+    self.dense2= nn.Linear(1024,256)
+    self.dense3= nn.Linear(256,100) #64 dimensional output
   
   def forward(self,x):
     x=self.pool1(F.relu(self.conv1(x)))
     x=self.pool1(F.relu(self.conv2(x)))
+    x=self.pool1(x)
     x = torch.flatten(x,1)
     x=F.relu(self.dense1(x))
     x=F.relu(self.dense2(x))
@@ -138,3 +138,10 @@ class speechEmbedded(nn.Module):
 
 class smallSpeechPrediction(nn.Module):
   # model for prediction from embedded model in 64 dimensions
+  def __init__(self):
+    super(smallSpeechPrediction,self).__init__()
+    self.dense0 = nn.Linear(100,32)
+  
+  def forward(self,x):
+    x = self.dense0(x)
+    return x
